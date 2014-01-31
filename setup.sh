859a371dd34d535a9d33557c20b047a06686ef1d
Charles H Martin <charlesmartin14@gmail.com>
2014-01-30 22:45:13 -0800

    creating lua setup

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+PKG_DIR = ~/packages
+
+
+# install lua 5.2
+# see http://stackoverflow.com/questions/5496003/how-do-i-install-lua-on-macos
+
+cd $PKG_DIR
+wget  http://www.lua.org/ftp/lua-5.2.3.tar.gz
+
+tar xvzf lua-5.2.3.tar.gz
+cd lua-5.2.3/src
+make macosx
+
+
+
+# install torch
+# see https://github.com/torch/torch7-distro
+
+cd $PKG_DIR
+git clone git://github.com/andresy/torch.git
+cd torch
+mkdir build
+cd build
+cmake .. 
+
